3a6d5761b419ea0d8c9cde3bca0a7c4431db0238
lucasliet <lucasouliveira@gmail.com>
2022-12-03 22:50:49 -0300

    enable cors

--- main.ts
+++ main.ts
@@ -1,25 +1,28 @@
 import opine, { json } from 'https://deno.land/x/opine@2.3.3/mod.ts';
-import { getStocks } from "./service/stockService.ts";
-import { Stock } from "./types/Stock.d.ts";
+import { opineCors } from 'https://deno.land/x/cors@v1.2.2/mod.ts';
+
+import { getStocks } from './service/stockService.ts';
+import { Stock } from './types/Stock.d.ts';
 
 const app = opine();
 
+app.use(opineCors());
 app.use(json());
 
 const stocks = await getStocks();
 
 console.info(`📈 collected ${stocks.length} stocks, ${JSON.stringify(stocks[0])}`,);
 
-app.get("/", (_, res) => res.send(stocks));
+app.get('/', (_, res) => res.json(stocks));
 
-app.get("/:paper", (req, res) => {
+app.get('/:paper', (req, res) => {
   const paper = req.params.paper.toUpperCase();
   const stock = stocks.find((stock: Stock) => stock.Papel === paper);
   if (stock) {
-    res.send(stock);
+    res.json(stock);
   } else {
     res.status = 404;
-    res.send({ error: `stock ${paper} not found` });
+    res.json({ error: `stock ${paper} not found` });
   }
 });
 
